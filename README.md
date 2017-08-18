8e046d72b8bc0751fefa6a1f51b2b986d045f225
Владимир Ренский <root@goblinzz.ru>
2017-08-18 13:32:20 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+import ipahttp
+
+host = 'ipamaster.example.com'
+user = 'user'
+passwd = 'passwd'
+ipa = ipahttp.ipa(host)
+ipa.login(user, passwd)
+print(ipa.dnsrecord_show(dns_zone,dns_name))
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='ipahttp',
+      version='0.3',
+      description='module for http API IPA',
+      url='https://github.com/pixonic/ipahttp',
+      author='Pixonic Team',
+      author_email='adm-srv@pixonic.com',
+      license='MIT',
+      packages=['ipahttp'],
+      install_requires=[
+            'requests',
+            'pyyaml'
+        ],
+      zip_safe=False)
